ffa237a353ce7beb9fe7501ef65c10a491907e42
Karla Gabriela Sandoval Balderas <166642284+KarlitaBalderas@users.noreply.github.com>
2025-11-14 14:04:23 -0600

    Update JugandoArreglos.java

--- jugandoArreglos/src/main/java/itszn/edu/mx/jugandoarreglos/JugandoArreglos.java
+++ jugandoArreglos/src/main/java/itszn/edu/mx/jugandoarreglos/JugandoArreglos.java
@@ -1,5 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystghjgkjhjem/Templates/Licenses/license-default.txt to change this license
  */
 
 package itszn.edu.mx.jugandoarreglos;
